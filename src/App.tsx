280d0bc688967da359724ff2fe8d7b71525b3137
Michael Haberler <git@mah.priv.at>
2024-09-14 15:59:08 +0200

    nested arrays

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,56 @@ import DateTime from './DateTime';
 import { useEffect, useState } from 'react';
 import { format } from 'date-fns';
 import { LatLng } from 'leaflet';
-import { fetchData } from './fetch';
+import { fetchData, fetchOpenMeteo } from './fetch';
+// import { fetchWeatherApi } from 'openmeteo';
 
 
+const levels = ["1000hPa",
+  "975hPa",
+  "950hPa",
+  "925hPa",
+  "900hPa",
+  "850hPa",
+  "800hPa",
+  "700hPa",
+  "600hPa",
+  "500hPa",
+  // "400hPa",
+  // "300hPa",
+  // "250hPa",
+  // "200hPa",
+  // "150hPa",
+  // "100hPa",
+  // "70hPa",
+  // "50hPa",
+  // "30hPa"
+]
+const values = ["temperature",
+  "relative_humidity",
+  "cloud_cover",
+  "wind_speed",
+  "wind_direction",
+  // "geopotential_height"
+] 
 
-// DateContext
 function App() {
   const [selectedDate, setSelectedDate] = useState<Date | null>(new Date());
   const [position, setPosition] = useState<LatLng>(new LatLng(47, 15));
   const [weatherData, setWeatherData] = useState({});
 
   useEffect(() => {
-    if (position) {
-      fetchData(
-        `https://api.open-meteo.com/v1/forecast?latitude=${position.lat}&longitude=${position.lng}&current=surface_pressure,temperature_2m,wind_speed_10m,weather_code,cloud_cover,relative_humidity_2m,is_day&hourly=temperature_2m&forecast_days=1`,
+    if (position && selectedDate) {
+
+      const params = {
+        "forecast_days": 1,
+        "latitude": position.lat,
+        "longitude": position.lng,
+        "hourly": values.map((v) => levels.map((l) => v + "_" + l)).flat()
+      };
+      const url = "https://api.open-meteo.com/v1/forecast";
+      fetchOpenMeteo(
+        url,
+        params,
         setWeatherData
       );
       console.log(weatherData);
@@ -43,11 +79,11 @@ function App() {
       </div>
       <div className="forecast">
         {
-          weatherData !== undefined ? weatherData?.hourly?.temperature_2m.map((_, key) =>
-            <div key={key}>
-              {key}h: {weatherData?.hourly.temperature_2m[key]}°
-            </div>
-          ) : 'None'
+          // weatherData !== undefined ? weatherData?.hourly?.temperature_2m.map((_, key) =>
+          //   <div key={key}>
+          //     {key}h: {weatherData?.hourly.temperature_2m[key]}°
+          //   </div>
+          // ) : 'None'
         }
       </div>
 
--- src/fetch.tsx
+++ src/fetch.tsx
@@ -4,3 +4,11 @@ export async function fetchData<T>(api: string, setState: React.Dispatch<React.S
     setState(data);
 }
 
+import { fetchWeatherApi } from 'openmeteo';
+
+export async function fetchOpenMeteo<T>(api: string, params: any, setState: React.Dispatch<React.SetStateAction<T>>): Promise<void> {
+    const response = await fetchWeatherApi(api, params);
+    const data = await response[0];
+    setState(data);
+}
+
